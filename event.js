7785f2f5e2b611299b6e9bbe47bafae693b984fd
Snooplobby <64603798+Snooplobby@users.noreply.github.com>
2020-04-30 13:07:02 -0600

    Add files via upload

---
+++ event.js
@@ -0,0 +1,33 @@
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "SportsEvent",
+  "name": "NHL Western Conference Final Playoff Tickets:",
+    "homeTeam": "Edmonton Oilers",
+    "awayTeam": "Calgary Flames",
+  "location": {
+    "@type": "Place",
+      "name": "Rodgers Place",
+    "address": {
+      "@type": "PostalAddress",
+      "addressLocality": "Edmonton",
+      "addressRegion": "AB"
+    },
+    "geo": {
+        "@type": "Geocoordinates",
+        "latitude": "53.5469097",
+        "longitude": "-113.4978413"
+        
+    },
+    "url": "https://goo.gl/maps/mQK8yLjoVocthCCi8"
+  },
+  "offers": {
+    "@type": "AggregateOffer",
+    "lowPrice": "$496",
+    "offerCount": "27"
+  },
+  "startDate": "2020-05-05T18:00",
+  "endDate": "2020-05-5T23:00",
+  "url": "http://www.ticketmaster.ca/Edmonton-oilers-tickets/artist/805943"
+}
+</script>
